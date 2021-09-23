d795950e96491ac1960acb2b80e81fb205207c1e
Jonathan Amaral <jonathancristovaolds@gmail.com>
2021-09-22 21:21:15 -0300

    Second Commit

---
+++ script.js
@@ -0,0 +1,102 @@
+
+
+const Crew1 = document.querySelector('.crew11')
+Crew1.addEventListener('click', () => PopUp1('PopUpOne'))
+
+ function PopUp1(PopUpOne){
+ const abrir = document.querySelector('.PopUpone')
+ abrir.classList.add('mudar')
+
+
+
+ }
+
+ 
+ 
+ const Crew11 = document.querySelector('.button1')
+ Crew11.addEventListener('click', () =>PopUp11 ('PopUpOne'))
+ 
+ function PopUp11(PopUpOne){
+     const fechar = document.querySelector('.PopUpone')
+     fechar.classList.remove('mudar')
+ }
+
+
+/* 2º Pop Up */
+
+
+const Dragon = document.querySelector('.dragon')
+Dragon.addEventListener('click', () => PopUp2('PopUpTwo'))
+
+ function PopUp2(PopUpTwo){
+ const abrir1 = document.querySelector('.PopUptwo')
+ abrir1.classList.add('mudar')
+
+ }
+ 
+
+
+
+ 
+ const Dragon11 = document.querySelector('.button2')
+ Dragon11.addEventListener('click', () =>PopUp22 ('PopUpTwo'))
+ 
+ function PopUp22(PopUpTwo){
+     const fechar2 = document.querySelector('.PopUptwo')
+     fechar2.classList.remove('mudar')
+ }
+ 
+ 
+
+/*3º Pop Up*/
+
+
+
+const Falcon = document.querySelector('.Falcon')
+Falcon.addEventListener('click', () => PopUp3('PopUpThree'))
+
+ function PopUp3(PopUpThree){
+ const abrir2 = document.querySelector('.PopUpthree')
+ abrir2.classList.add('mudar')
+
+ }
+ 
+
+
+
+ 
+ const Falcon1 = document.querySelector('.button3')
+ Falcon1.addEventListener('click', () =>PopUp33 ('PopUpThree'))
+ 
+ function PopUp33(PopUpThree){
+     const fechar3 = document.querySelector('.PopUpthree')
+     fechar3.classList.remove('mudar')
+ }
+
+
+
+ /*4º Pop Up*/
+
+
+
+const Starship = document.querySelector('.Starship')
+Starship.addEventListener('click', () => PopUp4('PopUpFour'))
+
+ function PopUp4(PopUpFour){
+ const abrir4 = document.querySelector('.PopUpfour')
+ abrir4.classList.add('mudar')
+
+ }
+ 
+ const Starship1 = document.querySelector('.button4')
+ Starship1.addEventListener('click', () =>PopUp44 ('PopUpFour'))
+ 
+ function PopUp44(PopUpFour){
+     const fechar4 = document.querySelector('.PopUpfour')
+     fechar4.classList.remove('mudar')
+ }
+
+
+ 
+
+
